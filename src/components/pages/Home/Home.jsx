83d39074fc578e7abbb62fdb0884384fa5c77cec
Md. Masud Rana <mr.hasib45@gmail.com>
2021-10-21 11:24:33 +0600

    make hpme page and header page

---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../ui/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>This is home</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/pages/NotFoundPage/index.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <Typography variant="h3" style={{color:"red", textAlign:"center"}}>404 Page Not Found</Typography>
+        </div>
+    );
+};
+
+export default NotFoundPage;
