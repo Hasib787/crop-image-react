20f196e9ec01ff40e19c51167ed1112545528bf0
Md. Masud Rana <mr.hasib45@gmail.com>
2021-10-21 12:36:19 +0600

    added header and tor scrool

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import NotFoundPage from "./components/pages/NotFoundPage";
+import ScrollTop from "./components/pages/ScrollTop";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Switch>
         
       <Route exact path="/" component={Home} />
-      
+      <Route exact path="/scrollTop" component={ScrollTop} />
 
       <Route path="*" component={NotFoundPage} />
         
--- src/components/ui/Header/index.jsx
+++ src/components/ui/Header/index.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import useStyles from "./Header.style";
 
 const Header = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <h2>This is  Header</h2>
-        </div>
+        <Box sx={{ flexGrow: 1 }} className={classes.headerRoot}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div"  >
+              Home
+            </Typography>
+            <Typography variant="h6" component="div" >
+              Upload Image
+            </Typography>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              About
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
     );
 };
 
